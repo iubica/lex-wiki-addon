33272b1990d8bd0ab44ff6f5093c49c99f8f8d96
Andrei Radulescu-Banu <bitdribble@gmail.com>
2015-06-02 19:37:02 -0400

    Development

--- lex-wiki/data/article-parser.js
+++ lex-wiki/data/article-parser.js
@@ -21,3 +21,29 @@ function lexWikiParseNewYorkTimesArticle() {
 
     self.postMessage(["New York Times", url, hdl, authors, date, descr]);    
 }
+
+function lexWikiParseWashingtonPostArticle() {
+    var url="", hdl="", authors="", date="", descr="";
+    var metas = document.getElementsByTagName("meta");
+    for (i=0; i<metas.length; i++) {
+	if (metas[i].getAttribute("property") == "og:url") {
+	    url = metas[i].getAttribute("content");
+	}
+	if (metas[i].getAttribute("property") == "og:title") {
+	    hdl = metas[i].getAttribute("content");
+	}
+	if (metas[i].getAttribute("name") == "author") {
+	    authors = metas[i].getAttribute("content");
+	}
+	if (metas[i].getAttribute("name") == "dat") {
+	    date = metas[i].getAttribute("content");
+	}
+	if (metas[i].getAttribute("property") == "twitter:description") {
+	    descr = metas[i].getAttribute("content");
+	}
+    }
+    
+    // Author, date, descr to be retrieved in a different way
+
+    self.postMessage(["New York Times", url, hdl, authors, date, descr]);    
+}
